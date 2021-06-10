07efefba29adf19ae32d722bf6a7fbb32062bc3d
Miguel Quiroz <sindro86@gmail.com>
2021-06-10 14:53:02 -0500

    Filled easy fields of the network inventory

--- webinars/web01/network_inventory.py
+++ webinars/web01/network_inventory.py
@@ -64,7 +64,7 @@ returns standardized output
 	#	'9.2(3)'	
 	# uptime: show_version[output][platform][kernel_uptime] 
 	# 	{'days': 0, 'hours': 0, 'minutes': 24, 'seconds': 29}
-	# serial number: show_inventory[hostname[output][name][Chassis][serial_number]
+	# serial number: show_inventory[hostname][output][name][Chassis][serial_number]
 	#	'9RDIN8H58L9'
 	#
 	ASA
@@ -90,25 +90,26 @@ def get_inventory(device, show_version, show_inventory):
 	device_os = device.os
 
 	if device_os == 'iosxr':
-		software_version = None
-		uptime = None
-		serial_number = None
+		software_version = show_version[hostname]['output']['software_version']
+		uptime = show_version[hostname]['output']['uptime']
+		serial_number = 'missing'
 	elif device_os == 'iosxe':
-		software_version = None
-		uptime = None
-		serial_number = None
+		model = show_version[hostname]['output']['version']['chassis']
+		software_version = show_version[hostname]['output']['version']['version']
+		uptime = show_version[hostname]['output']['version']['uptime']
+		serial_number = show_inventory[hostname]['output']['main']['chassis'][model]['sn']
 	elif device_os == 'nxos':
-		software_version = None
-		uptime = None
-		serial_number = None
+		software_version = show_version[hostname]['output']['platform']['software']['system_version']
+		uptime = show_version[hostname]['output']['platform']['kernel_uptime']
+		serial_number = show_inventory[hostname]['output']['name']['Chassis']['serial_number']
 	elif device_os == 'asa':
 		software_version = None
 		uptime = None
-		serial_number = None
-	elif device_os == 'iosxr':
-		software_version = None
-		uptime = None
-		serial_number = None
+		serial_number = show_inventory[hostname]['output']['Chassis']['sn']
+	elif device_os == 'ios':
+		software_version = show_version[hostname]['output']['version']['version']
+		uptime = show_version[hostname]['output']['version']['uptime']
+		serial_number = show_version[hostname]['output']['version']['chassis_sn']
 	else:
 		return False
 
@@ -125,6 +126,9 @@ if __name__ == '__main__':
 	# Empty dictionary to store the output of the 'show inventory' command
 	# { hostname : {type : output} }
 	show_inventory = {}
+
+	# List of fields in network inventory report
+	network_inventory = []
 
 	# Read testbed filename
 	parser = argparse.ArgumentParser(description='testing pyATS')
@@ -143,18 +147,18 @@ if __name__ == '__main__':
 	for device in testbed.devices.values():
 		# Run commands to gather information from network device
 		show_version[device.name] = parse_command(device,'show version')
-		# pprint(show_version[device.name])
-
 		show_inventory[device.name] = parse_command(device, 'show inventory')
-		# pprint(show_inventory[device.name])
 
 		# Build network inventory
-		get_inventory(device, show_inventory, show_version)
+		print(get_inventory(device, show_version, show_inventory))
+		#network_inventory.append(get_inventory(device, show_version, show_inventory))
 		
 		# Disconnect from device
 		device.disconnect()
 		print(f'Disconnected successfully from {device.name}')
 
-
+	# uncomment the following to see the data structure
+	# pprint(show_version)
+	# pprint(show_inventory)
 	
 	# Write to a CSV file
