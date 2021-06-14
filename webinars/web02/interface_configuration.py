360a7291d9e5a7cf65be39defcab911a16af8bd2
Miguel Quiroz <sindro86@gmail.com>
2021-06-14 13:12:44 -0500

    Reading command line arguments

--- webinars/web02/interface_configuration.py
+++ webinars/web02/interface_configuration.py
@@ -10,10 +10,30 @@ Goal:
 """
 
 if __name__ == '__main__':
-	print("Updating interfaces' description")
-	# Load testbed file
+	import argparse
+	
+	# Read command line arguments
+	parser = argparse.ArgumentParser(
+		description='Updating interface descriptions')
+	parser.add_argument(
+		'--testbed', required=True, type=str, help='Testbed filename')
+	parser.add_argument(
+		'--sot', required= True, type=str, help='Source of truth filename')
+	parser.add_argument(
+		'--apply', action='store_true', help='If set, configurations are applied.')
+	
+	args = parser.parse_args()
+
 	# Read the source of truth file
+	print(f'Reading {args.sot}')
+
 	# Create interface descriptions
+
+	# Load testbed file
+	print(f'Loading {args.testbed}')
+	if args.apply:
+		print('Applying configurations')
+	
 	# Connect to all devices
 	# Grab current interface descriptions
 	# Apply new interface descriptions
